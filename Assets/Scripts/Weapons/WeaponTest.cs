450803e56f3d5ac565793ff36ad5495782317114
Lukas Chodosevicius <lukaschodosevicius@gmail.com>
2017-01-21 21:36:40 +0200

    Adding ignore and removing ignored files

--- Assets/Scripts/Weapons/WeaponTest.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class WeaponTest : ProjectileWeapon
-{
-	public override int MagazineCapacity { get { return 15; } }
-	public override float ReloadDuration { get { return 1; } }
-	public override float FireDuration { get {return 0.1f; } }
-}
